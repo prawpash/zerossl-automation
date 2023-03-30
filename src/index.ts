73a61087093dfb4942b58eda3c24ad73639e2711
prawira <robbyprawira3@gmail.com>
2023-03-30 15:45:13 +0700

    👔 Add Main Function
    Add Main function to run first when app executed

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,6 @@
 import { pino } from "pino";
+import yargs from "yargs";
+import { hideBin } from "yargs/helpers";
 
 const logger = pino({
   transport: {
@@ -19,3 +21,18 @@ const logger = pino({
     ],
   },
 });
+const main = async () => {
+  try {
+    const { domain, csrPath, projectDir } = await yargs(hideBin(process.argv))
+      .usage(
+        "Usage: $0 --csr-path=[string] --domain=[string] --project-dir=[string]"
+      )
+      .string(["domain", "csr-path", "project-dir"])
+      .demandOption(["domain", "csr-path", "project-dir"]).argv;
+
+  } catch (error: any) {
+    logger.error(error);
+  }
+};
+
+main();
